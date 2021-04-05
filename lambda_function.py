8aa6665a7717150c0614f679146945d8fd981e6f
Yinlin Chen <ylchen@vt.edu>
2021-04-05 14:35:59 -0400

    code update

--- lambda_function.py
+++ lambda_function.py
@@ -2,30 +2,50 @@ import boto3
 import json
 import os
 
-# Environment variables
 region_name = os.getenv('Region')
 source_table_name = os.getenv('Source_Table')
 target_table_name = os.getenv('Target_Table')
-
-try:
-    dyndb = boto3.resource('dynamodb', region_name=region_name)
-    source_table = dyndb.Table(source_table_name)
-    target_table = dyndb.Table(target_table_name)
-
-    response = source_table.scan()
-    source_table_items = response['Items']
-
-    while response.get('LastEvaluatedKey', False):
-        response = source_table.scan(
-            ExclusiveStartKey=response['LastEvaluatedKey'])
-        source_table_items.extend(response['Items'])
-
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
-    raise e
+dyndb = boto3.resource('dynamodb', region_name=region_name)
 
 
 def lambda_handler(event, context):
+
+    print("From table: " + source_table_name)
+    print("To table: " + target_table_name)
+    source_table_items = fetchRecords(source_table_name)
+    insertRecords(source_table_items, target_table_name)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Table is copied.",
+        }),
+    }
+
+
+def fetchRecords(source_table_name):
+
+    try:
+
+        source_table = dyndb.Table(source_table_name)
+        response = source_table.scan()
+        source_table_items = response['Items']
+
+        while response.get('LastEvaluatedKey', False):
+            response = source_table.scan(
+                ExclusiveStartKey=response['LastEvaluatedKey'])
+            source_table_items.extend(response['Items'])
+
+        return source_table_items
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        raise e
+
+
+def insertRecords(source_table_items, target_table_name):
+
+    target_table = dyndb.Table(target_table_name)
 
     for item in source_table_items:
         try:
@@ -35,10 +55,3 @@ def lambda_handler(event, context):
         except Exception as e:
             print(f"An error occurred: {str(e)}")
             raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Table copied.",
-        }),
-    }
